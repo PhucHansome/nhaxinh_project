62e0199485cc613551d5a03d3f2aa35f8672a8be
Phuchandsome <phucnguyenksqt11@gmail.com>
2022-08-21 13:14:45 +0700

    update search

--- project/src/main/java/com/cg/security/SecurityConfig.java
+++ project/src/main/java/com/cg/security/SecurityConfig.java
@@ -61,10 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic().authenticationEntryPoint(restServicesEntryPoint());
 
         http.authorizeRequests()
-                .antMatchers("/", "/detail","/login", "/search", "/login_admin","/productdashboard", "/homedashboard","/api/auth/register","/homedashboard","/api/auth/login").permitAll()
-
-//                .antMatchers("/", "/detail","/login", "/search","/productdashboard", "/homedashboard","/api/auth/login","/dashboardLogin").permitAll()
-
+                .antMatchers("/", "/detail","/login", "/search", "/login_admin","/api/auth/register","/homedashboard","/api/auth/login").permitAll()
 //                .antMatchers("/order", "/user", "/product").hasAnyAuthority("ADMIN")
                 .antMatchers("/assets/**","/assets_dashboard/**", "/META-INF").permitAll()
                 .antMatchers("https://nhaxinh.com/public/asset/**").permitAll()
