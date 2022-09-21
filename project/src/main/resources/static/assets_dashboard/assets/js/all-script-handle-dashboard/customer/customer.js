b6d49410137e54f142373e53a95caf7ec153e17e
huynhvinh8398 <huynhvanvinh080398@gmail.com>
2022-09-21 16:12:50 +0700

    vinh

--- project/src/main/resources/static/assets_dashboard/assets/js/all-script-handle-dashboard/customer/customer.js
+++ project/src/main/resources/static/assets_dashboard/assets/js/all-script-handle-dashboard/customer/customer.js
@@ -44,63 +44,9 @@
             console.log(data)
         })
     }
-    page.dialogs.commands.handleActive = () =>{
-        $("#btn-active-block").on("click",() =>{
-            console.log("vô đây chưa");
-                let id =$(this).data("id")
-            console.log(id)
-                page.dialogs.commands.getCustomerInfoById(id).then(() =>{
-                    let currentRow = $('#tr_' + customerInfo.id);
-                    let updateRow = `
-                     <tr id="tr_${customerInfo.id}">
-                        <td></td>
-                        <td>${customerInfo.phone}</td>
-                        <td>${customerInfo.fullName}</td>
-                        <td>${customerInfo.userName}</td>
-                        <td>
-                        <button class="btn btn-outline-danger" type="button" data-id="${customerInfo.id}" id="btn-block"><i class="fas fa-lock"></i></button>
-                        </td>                 
-                    </tr>
-                     `
-                    App.IziToast.showSuccessAlert("Tài khoản đã bị block");
-                    currentRow.replaceWith(updateRow);
-                    page.dialogs.commands.handleBlock()
 
-                })
-        })
-    }
-
-    page.dialogs.commands.handleBlock =() =>{
-        $("#btn-block").on("click",() => {
-            let id = $(this).data("id");
-            console.log(id)
-            page.dialogs.commands.getCustomerInfoById(id).then(() => {
-
-                let currentRow1 = $('#tr_' + customerInfo.id);
-                let updateRow1 = `
-
-                     <tr id="tr_${customerInfo.id}">
-                        <td></td>
-                        <td>${customerInfo.phone}</td>
-                        <td>${customerInfo.fullName}</td>
-                        <td>${customerInfo.userName}</td>
-                        <td>
-                            <button data-id="${customerInfo.id}" class="btn btn-outline-success" type="button"  id="btn-active-block"></button>
-                            
-                        </td>
-                    </tr>
-
-            `
-                App.IziToast.showSuccessAlert("Tài khoản đã mở khóa");
-                currentRow1.replaceWith(updateRow1);
-
-                page.dialogs.commands.handleActive();
-            })
-        })
-    }
 
     $(function (){
     page.dialogs.commands.goCreateCustomer();
-        page.dialogs.commands.handleActive();
-        page.dialogs.commands.handleBlock();
+
 })
