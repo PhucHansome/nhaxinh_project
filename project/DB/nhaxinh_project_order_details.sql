bbebd453887390c7bc09364d3d5b483a3ccd8067
huynhvinh8398 <huynhvanvinh080398@gmail.com>
2022-10-05 17:08:25 +0700

    vinh

---
+++ project/DB/nhaxinh_project_order_details.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: nhaxinh_project
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `order_details`
+--
+
+LOCK TABLES `order_details` WRITE;
+/*!40000 ALTER TABLE `order_details` DISABLE KEYS */;
+INSERT INTO `order_details` VALUES (18,'2022-08-15 21:01:52.011000',NULL,0,'2022-09-21 08:33:40.321000',NULL,'Boris Weeks',35751000,'Đã giao hàng thành công','54 Green Milton Road','Huyện Bình Giang','+1 (687) 264-7879','Tỉnh Hải Dương','vinh123@cv.cc',NULL),(19,'2022-08-12 21:02:25.657000',NULL,0,'2022-09-21 08:41:37.596000',NULL,'Grant Huber',14655000,'Đã giao hàng thành công','20 East Hague Road','Huyện  Vũng Liêm','+1 (325) 965-4656','Tỉnh Vĩnh Long','vinh123@cv.cc',NULL),(20,'2022-06-12 21:02:53.619000',NULL,0,'2022-10-03 08:22:13.821000',NULL,'Raven Daugherty',14750000,'Đang giao hàng','525 South Oak Drive','Huyện An Phú','+1 (926) 785-6017','Tỉnh An Giang','vinh123@cv.cc',NULL),(21,'2022-05-12 22:44:08.561000',NULL,0,'2022-09-12 22:45:23.519000',NULL,'Nicholas Marsh',14560000,'Đơn hàng đã duyệt','23 South Oak Extension','Huyện Bảo Lâm','+1 (276) 983-6322','Tỉnh Lâm Đồng','vinh123@cv.cc',NULL),(22,'2022-03-12 22:44:47.354000',NULL,0,'2022-09-12 22:45:16.582000',NULL,'Xena Harvey',44345000,'Đơn hàng đã duyệt','85 Green First Street','Huyện Bắc Trà My','+1 (292) 835-4859','Tỉnh Quảng Nam','vinh123@cv.cc',NULL),(23,'2022-02-13 10:40:48.981000',NULL,0,'2022-10-03 14:54:19.845000',NULL,'Aubrey Young',88128000,'Đang giao hàng','43 Cowley Avenue','Huyện Cao Phong','+1 (544) 881-5932','Tỉnh Hoà Bình','vinh123@cv.cc',NULL),(24,'2021-01-13 10:42:32.068000',NULL,0,'2022-09-26 10:14:29.086000',NULL,'Hadassah Campbell',211392000,'Đã giao hàng thành công','18 West Rocky Second Parkway','Huyện Càng Long','+1 (688) 211-6366','Tỉnh Trà Vinh','vinh123@cv.cc',NULL),(31,'2022-09-15 11:32:38.947000',NULL,0,'2022-09-21 08:45:14.627000',NULL,'Madonna Hammond',6335000,'Đã giao hàng thành công','78 First Boulevard','Huyện Chư Păh','+1 (166) 826-5722','Tỉnh Gia Lai','vinh123@cv.cc',NULL),(35,'2022-09-19 09:19:15.152000',NULL,0,'2022-07-19 09:19:59.717000',NULL,'Madonna Hammond',24565000,'Đã Hủy đơn hàng','78 First Boulevard','Huyện Chư Păh','+1 (166) 826-5722','Tỉnh Gia Lai','vinh123@cv.cc',NULL),(36,'2022-09-26 09:46:21.442000',NULL,0,'2022-09-26 09:47:38.964000',NULL,'Mollie Sanchez',24565000,'Đã giao hàng thành công','966 Green New Extension','Huyện Bình Giang','0834789632','Tỉnh Hải Dương','tatipideqa@mailinator.com',NULL),(37,'2022-09-26 10:50:03.108000',NULL,0,'2022-10-02 18:41:58.920000',NULL,'Huỳnh Văn Vinh',73695000,'Đã giao hàng thành công','Lí Thái Tổ','Thành phố Huế','0839707285','Tỉnh Thừa Thiên Huế','vinh123@cv.cc',NULL),(38,'2022-09-26 10:52:27.079000',NULL,0,'2022-09-26 16:59:30.603000',NULL,'Huỳnh Văn Vinh',23488000,'Đã giao hàng thành công','Lí Thái Tổ','Thành phố Huế','0839707285','Tỉnh Thừa Thiên Huế','vinh123@cv.cc',NULL),(39,'2022-09-26 16:50:38.093000',NULL,0,'2022-09-26 16:57:11.800000',NULL,'Curran',67552000,'Đơn hàng đã duyệt','80 East White Milton Boulevard','Huyện Bảo Lâm','0254713254','Tỉnh Lâm Đồng','cefy@mailinator.com',NULL),(40,'2022-10-03 08:20:42.155000',NULL,0,'2022-10-03 08:21:50.839000',NULL,'Huỳnh Văn Vinh',70464000,'Đã giao hàng thành công','Lí Thái Tổ','Thành phố Huế','0839707285','Tỉnh Thừa Thiên Huế','vinh123@cv.cc',NULL),(41,'2022-10-03 11:37:44.747000',NULL,0,'2022-10-03 11:37:45.008000',NULL,'Huỳnh Văn Vinh',3848000,'Đang chờ duyệt','Lí Thái Tổ','Thành phố Huế','0839707285','Tỉnh Thừa Thiên Huế','vinh123@cv.cc',NULL),(42,'2022-10-03 15:09:38.183000',NULL,0,'2022-10-03 15:09:38.418000',NULL,'Huỳnh Văn Vinh',39253000,'Đang giao hàng','Lí Thái Tổ','Thành phố Huế','0839707285','Tỉnh Thừa Thiên Huế','vinh123@cv.cc',NULL),(43,'2022-10-03 15:11:49.613000',NULL,0,'2022-10-03 15:11:49.680000',NULL,'Mollie Sanchez',35334501,'Đang giao hàng','966 Green New Extension','Huyện Bình Giang','0834789632','Tỉnh Hải Dương','vinh123@cv.cc',NULL),(44,'2022-10-03 15:22:16.900000',NULL,0,'2022-10-03 15:22:29.000000',NULL,'dương văn kiềm',16768000,'Đã Hủy đơn hàng','huế','Huyện Cờ Đỏ','0125489654','Thành phố Cần Thơ','vinh123@cv.cc',NULL),(45,'2022-10-03 16:57:40.365000',NULL,0,'2022-10-03 16:57:57.659000',NULL,'Huỳnh Văn Vinh',93952000,'Đã giao hàng thành công','Lí Thái Tổ','Thành phố Huế','0839707285','Tỉnh Thừa Thiên Huế','vinh123@cv.cc',NULL),(46,'2022-10-04 09:54:14.013000',NULL,0,'2022-10-04 09:54:14.122000',NULL,'Huỳnh Văn Vinh',2080000,'Đang giao hàng','Lí Thái Tổ','Thành phố Huế','0839707285','Tỉnh Thừa Thiên Huế','vinh123@cv.cc',NULL),(47,'2022-10-04 09:56:45.456000',NULL,0,'2022-10-04 09:56:45.473000',NULL,'Huỳnh Văn Vinh',29376000,'Đang giao hàng','Lí Thái Tổ','Thành phố Huế','0839707285','Tỉnh Thừa Thiên Huế','vinh123@cv.cc',NULL),(49,'2022-10-04 10:14:57.504000',NULL,0,'2022-10-04 10:14:57.596000',NULL,'Jorden Dunn',39253000,'Đang giao hàng','90 South Rocky Second Boulevard','Huyện Đình Lập','0125478965','Tỉnh Lạng Sơn','vinh123@cv.cc',NULL),(50,'2022-10-04 15:14:51.225000',NULL,0,'2022-10-04 15:14:51.436000',NULL,'Huỳnh Văn Vinh',23488000,'Đang giao hàng','Lí Thái Tổ','Thành phố Huế','0839707285','Tỉnh Thừa Thiên Huế','vinh123@cv.cc',NULL),(51,'2022-10-04 16:03:07.040000',NULL,0,'2022-10-04 16:03:07.140000',NULL,'Ursula Gallegossssssss',4160000,'Đang giao hàng','Aute aliquip quo vol','Huyện Cao Lãnh','+1 (223) 293-7503','Tỉnh Đồng Tháp','vinh123@cv.cc',NULL),(53,'2022-10-04 16:56:21.934000',NULL,0,'2022-10-04 16:56:41.277000',NULL,'Huỳnh Văn Vinh',39253858,'Đã giao hàng thành công','Lí Thái Tổ','Thành phố Huế','0839707285','Tỉnh Thừa Thiên Huế','vinh123@cv.cc',NULL),(56,'2022-10-04 16:58:56.114000',NULL,0,'2022-10-04 16:59:14.177000',NULL,'Huỳnh Văn Vinh',29376000,'Đã giao hàng thành công','Lí Thái Tổ','Thành phố Huế','0839707285','Tỉnh Thừa Thiên Huế','vinh123@cv.cc',NULL),(68,'2022-10-04 17:02:45.624000',NULL,0,'2022-10-04 17:02:45.656000',NULL,'Ursula Gallegossssssss',14688000,'Đang giao hàng','Aute aliquip quo vol','Huyện Cao Lãnh','+1 (223) 293-7503','Tỉnh Đồng Tháp','vinh123@cv.cc',NULL);
+/*!40000 ALTER TABLE `order_details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-05 17:05:17
---
+++ project/DB/nhaxinh_project_orders.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: nhaxinh_project
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (11,'2021-09-09 21:01:52.044000',NULL,0,'2022-09-21 08:33:40.150000',NULL,'Aut autem aliquip of',23488000,'57e85821-f0ae-4761-bc58-8431b9883728','KS-3*23654',1,'https://res.cloudinary.com/phucnguyenksqt11/image/upload/v1662693911/Product_Image_Nhaxinh/4069dcca-2212-4155-a144-5718b7501e67.jpg','Kệ Sách Artigo','Đã giao hàng thành công',18),(12,'2022-01-10 21:01:52.122000',NULL,0,'2022-09-21 08:33:40.231000',NULL,'Aut autem aliquip of',2080000,'57e85821-f0ae-4761-bc58-8431b9883728','DH-3*54231',1,'https://res.cloudinary.com/phucnguyenksqt11/image/upload/v1662694168/Product_Image_Nhaxinh/92502fcb-ef04-4f05-bb70-8c41d821e853.gif','Đồng hồ để bàn Terra','Đã giao hàng thành công',18),(13,'2022-02-12 21:01:52.148000',NULL,0,'2022-09-21 08:33:40.245000',NULL,'Aut autem aliquip of',6335000,'57e85821-f0ae-4761-bc58-8431b9883728','TA-3*42152',1,'https://res.cloudinary.com/phucnguyenksqt11/image/upload/v1662694458/Product_Image_Nhaxinh/d75b741b-5ce5-44dd-966a-99ee1666f0c1.jpg','Tủ áo Harmony','Đã giao hàng thành công',18),(37,'2022-10-03 15:22:16.963000',NULL,0,'2022-10-03 15:22:28.984000',NULL,'',14688000,'83c66dd4-c23c-4a0d-b131-8cd9906f0dc6','GL-31*12342123',1,'https://res.cloudinary.com/phucnguyenksqt11/image/upload/v1664506360/Product_Image_Nhaxinh/0e8a3c58-fe07-43b8-ac7b-2ba3906c98fd.jpg','Ghế xoay Basil Cognac','Đã Hủy đơn hàng',44),(38,'2022-10-03 16:57:40.424000',NULL,0,'2022-10-03 16:57:57.540000',NULL,'',93952000,'57e85821-f0ae-4761-bc58-8431b9883728','KS-3*23654',4,'https://res.cloudinary.com/phucnguyenksqt11/image/upload/v1663051580/Product_Image_Nhaxinh/4faea8c1-21ff-4463-8742-d3c0ea24ad67.jpg','Kệ Sách Artigo','Đã giao hàng thành công',45),(39,'2022-10-04 09:54:14.091000',NULL,0,'2022-10-04 09:54:14.091000',NULL,'',2080000,'57e85821-f0ae-4761-bc58-8431b9883728','DH-3*54231',1,'https://res.cloudinary.com/phucnguyenksqt11/image/upload/v1662694168/Product_Image_Nhaxinh/92502fcb-ef04-4f05-bb70-8c41d821e853.gif','Đồng hồ để bàn Terra','Đang giao hàng',46),(43,'2022-10-04 10:14:57.588000',NULL,0,'2022-10-04 10:14:57.588000',NULL,'',24565000,'6dbb53fa-021a-438a-9675-c2dca9eb0263','SF-31*12345638',1,'https://res.cloudinary.com/phucnguyenksqt11/image/upload/v1663665412/Product_Image_Nhaxinh/71958988-24d8-414d-b6ee-356a83cedd23.jpg','Sofa 3 chỗ Osaka mẫu 1 vải 29','Đang giao hàng',49),(45,'2022-10-04 16:03:07.107000',NULL,0,'2022-10-04 16:03:07.107000',NULL,'',4160000,'59a67091-a9c7-43eb-9ac8-0111bc29dbf9','DH-3*54231',2,'https://res.cloudinary.com/phucnguyenksqt11/image/upload/v1662694168/Product_Image_Nhaxinh/92502fcb-ef04-4f05-bb70-8c41d821e853.gif','Đồng hồ để bàn Terra','Đang giao hàng',51),(49,'2022-10-04 16:56:22.062000',NULL,0,'2022-10-04 16:56:41.246000',NULL,'',858,'57e85821-f0ae-4761-bc58-8431b9883728','VA-31*68452321',1,'https://res.cloudinary.com/phucnguyenksqt11/image/upload/v1664506739/Product_Image_Nhaxinh/ef350b06-9240-4229-82d7-f3f42762d387.jpg','Drake Mcpherson','Đã giao hàng thành công',53),(50,'2022-10-04 16:56:22.068000',NULL,0,'2022-10-04 16:56:41.256000',NULL,'',24565000,'57e85821-f0ae-4761-bc58-8431b9883728','SF-31*12345638',1,'https://res.cloudinary.com/phucnguyenksqt11/image/upload/v1663665412/Product_Image_Nhaxinh/71958988-24d8-414d-b6ee-356a83cedd23.jpg','Sofa 3 chỗ Osaka mẫu 1 vải 29','Đã giao hàng thành công',53),(52,'2022-10-04 16:58:56.249000',NULL,0,'2022-10-04 16:59:14.115000',NULL,'',29376000,'57e85821-f0ae-4761-bc58-8431b9883728','GL-31*12342123',2,'https://res.cloudinary.com/phucnguyenksqt11/image/upload/v1664506360/Product_Image_Nhaxinh/0e8a3c58-fe07-43b8-ac7b-2ba3906c98fd.jpg','Ghế xoay Basil Cognac','Đã giao hàng thành công',56);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-05 17:05:19
